8007853aea7e64f6b157c62989db30d99293a036
Gregory Larkin <gregory.b.larkin@gmail.com>
2013-05-28 16:00:14 -0500

    I did something

---
+++ KCX/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KCX::Application.config.secret_token = '1c01db522ef0168174edd6244ea836cc12cc97081838b899e87c3b702f661b8760d27f89016f2de6bce07c840073e73d463397db92eb2f40094410f9466451bb'
