4431b018d17295543e138c4344bb0daf53d5b909
Peter McKelvy <peter.mckelvy@gmail.com>
2013-06-07 16:10:13 -0500

    Please don't delete the app

--- KCX/app/models/user.rb
+++ KCX/app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   has_many :costume
   has_secure_password
 
+  validates :email, presence: true, uniqueness: true
+  validates :phone, presnece: true, uniqueness: true
 end
